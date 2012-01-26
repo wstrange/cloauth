1c7d4c5e04a16fef2387f16aeae40ec17d5d6d9b
Warren Strange <warren.strange@gmail.com>
2012-01-26 12:02:57 -0700

    h

--- src/cloauth/server.clj
+++ src/cloauth/server.clj
@@ -1,6 +1,9 @@
 (ns cloauth.server
-  (:require [noir.server :as server])
-  (:use korma.db ))
+  (:require [noir.server :as server]
+            [cloauth.models.kdb :as db]
+             [clojure.string :as str] )
+  (:use korma.db )
+  (:import (java.net URI)))
 
 (comment
 (defdb db (mysql {:db "cloauth"
@@ -8,7 +11,7 @@
                   :port "3306"
                   :delimiters "`"
                   :user "cloauth"
-                  :password "password"})))
+                  :password "password"}))
 
 
 
@@ -20,13 +23,40 @@
            ; else
            {:user "cloauth"
             :subname "//localhost:5432/cloauth"
-            :password "password"})))
+            :password "password"}))))
+
+
  
+(defn heroku-db
+  "Generate the db map according to Heroku environment when available."
+  []
+  (when (System/getenv "DATABASE_URL")
+    (let [url (URI. (System/getenv "DATABASE_URL"))
+          host (.getHost url)
+          port (if (pos? (.getPort url)) (.getPort url) 5432)
+          path (.getPath url)]
+      (merge
+       {:subname (str "//" host ":" port path)}
+       (when-let [user-info (.getUserInfo url)]
+         {:user (first (str/split user-info #":"))
+          :password (second (str/split user-info #":"))})))))
+(def db-params
+  (merge {:classname "org.postgresql.Driver"
+          :subprotocol "postgresql"
+          :subname "//localhost:5432/cloauth"
+          :user "cloauth"
+          :password "password"}
+         (heroku-db)))
+
+
  
 (defdb db db-params)
 
 (println "Db defined " db)
 (println "DB params " db-params)
+; run test db select 
+(println "test user select" (db/all-users)) 
+
 
 (def port (Integer. (get (System/getenv) "PORT" "8080")))
                  
